c6031f503d539e881ab3f1574f97c3e5fe50b1ea
Marvin Drude <marvindrude1@gmail.com>
2026-04-01 23:13:13 +0200

    Update `TypeIdGenerator` to version `1.0.6` and refine project configurations
    - Bumped `GeneratorVersion`, `AssemblyVersion`, `FileVersion`, and `Version` to `1.0.6`.
    - Enhanced package generation settings with `CopyLocalLockFileAssemblies` and new dependency management targets.
    - Added support for `Me.Memory` package and updated analyzer packaging configurations.

--- Generators/Beskar.CodeGeneration.TypeIdGenerator/TypeIdGenerator.cs
+++ Generators/Beskar.CodeGeneration.TypeIdGenerator/TypeIdGenerator.cs
@@ -6,7 +6,7 @@ namespace Beskar.CodeGeneration.TypeIdGenerator;
 public sealed partial class TypeIdGenerator : IIncrementalGenerator
 {
    public const string GeneratorName = "TypeIdGenerator";
-   public const string GeneratorVersion = "1.0.2";
+   public const string GeneratorVersion = "1.0.6";
    
    public void Initialize(IncrementalGeneratorInitializationContext context)
    {
